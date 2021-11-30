d766d37612c8ad40c21c61da440bae10be776815
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-11-30 04:26:14 +0100

    why

--- src/main/java/Saddad.java
+++ src/main/java/Saddad.java
@@ -124,21 +124,23 @@ public class Saddad {
             if (limit.get()){
                 System.out.println("nsfw: " + nsfwCount + " sfw: " + sfwCount + " total: " + (nsfwCount + sfwCount) + " threads: " + ((ThreadPoolExecutor) Pool.service).getPoolSize());
                 System.err.println("Limit reached");
-                Pool.service.shutdown();
                 Pool.parallelAsync.shutdown();
+                Pool.service.shutdown();
+    
                 System.err.println("Awaiting termination Timeout: 2 minutes");
-                try {
-                    boolean b = Pool.service.awaitTermination(1, TimeUnit.MINUTES);
-                    if (!b) System.err.println("Timeout while awaiting service termination");
-                }catch(InterruptedException e){
-                    System.err.println("Interrupted while awaiting service termination");
-                }
                 try {
                     boolean b = Pool.parallelAsync.awaitTermination(1, TimeUnit.MINUTES);
                     if (!b) System.err.println("Timeout while awaiting parallelAsync termination");
                 }catch(InterruptedException e){
                     System.err.println("Interrupted while awaiting parallelAsync termination");
                 }
+                try {
+                    boolean b = Pool.service.awaitTermination(1, TimeUnit.MINUTES);
+                    if (!b) System.err.println("Timeout while awaiting service termination");
+                }catch(InterruptedException e){
+                    System.err.println("Interrupted while awaiting service termination");
+                }
+    
                 savingScrapper.start();
                 break;
             }
